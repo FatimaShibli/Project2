77f278134a4b454099d55a0b0c95b57fcec9f761
Fatima Shibli <95264693+FatimaShibli@users.noreply.github.com>
2023-05-10 18:54:06 -0500

    Update ls.go

--- Project2/builtins/ls.go
+++ Project2/builtins/ls.go
@@ -3,15 +3,17 @@ package builtins
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
+// List returns a list of files in the current directory.
 func List(args ...string) error {
 	files, err := os.ReadDir(".")
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read directory: %v", err)
 	}
 	for _, file := range files {
-		fmt.Println(file.Name())
+		fmt.Println(filepath.Join(".", file.Name()))
 	}
 	return nil
 }
